7d68d546966b4000eb955866631705ff4e5c3f67
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-09-14 22:09:11 +0100

    Creates basic routes structure (Games and Ads)

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,33 @@ import express from 'express';
 
 const app = express();
 
-// Route
-app.get('/ads', (request, response) => {
+// List games
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+// Create ad
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+// List ads by game
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Ad 1' },
     { id: 2, name: 'Ad 2' },
     { id: 3, name: 'Ad 3' },
     { id: 4, name: 'Ad 4' },
   ]);
+});
+
+// List discord users by ad
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
